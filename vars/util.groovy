b4e20e472998ba9a2e2a9cb99c21c87add5c0b06
Gabriel Oliveira <oliveira992gabriel@gmail.com>
2022-06-06 08:49:31 -0300

    add files devops v1

--- vars/util.groovy
+++ vars/util.groovy
@@ -1,13 +1,5 @@
 def call(repo, branch) {
-    steps {
-
-        git(
-            url: "${repo}",
-            credentialsId: 'github_user',
-            branch: "${branch}"
-        )
-        
-    }
+    echo "ok"
 }
 
   
